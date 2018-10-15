3531780b7b16bafe08541720ca3537a78dadb4da
Tyler Brothers <tylerbrothers1@gmail.com>
2018-10-15 15:10:29 -0600

    Relative pathing to to_json.lua

--- lib/censusplusreader.rb
+++ lib/censusplusreader.rb
@@ -2,6 +2,6 @@ require "censusplusreader/version"
 
 module Censusplusreader
   def self.to_json(path_to_lua)
-    JSON.parse(`lua to_json.lua #{path_to_lua} CensusPlus_Database`)
+    JSON.parse(`lua #{File.dirname(__FILE__) + '/to_json.lua' } #{path_to_lua} CensusPlus_Database`)
   end
 end
